263fe2a4954f3175f1c913b86f4e223f6b52cf15
msahlaoui <msahlaoui@gmail.com>
2018-07-03 08:02:02 +0000

    First commit

---
+++ constants/db_config.php
@@ -0,0 +1,6 @@
+<?php
+$servername = "172.21.242.169";
+$username = "hscroot";
+$password = "Azerty$1";
+$dbname = "butineo";
+?>
---
+++ constants/settings.php
@@ -0,0 +1,14 @@
+<?php
+$actual_link = $_SERVER['HTTP_HOST'];
+
+$default_timezone = 'America/Los_Angeles';
+$smtp_host = '';
+$smtp_user = '';
+$smtp_pass = '';
+
+$contact_mail = 'msahlaoui@gmail.com';
+
+$fb  = '#';
+$tw = '#';
+$ig = '#';
+?>
